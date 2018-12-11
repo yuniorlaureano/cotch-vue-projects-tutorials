c8b2cf275fc434b1e884064045f1cbce21d4342a
Yunior Miguel Laureano <yu.laureano@dga.gov.do>
2018-12-11 08:42:30 -0400

    Rutas con el signo de numero, el por defecto utilizado y que no da problemas, pero con el posicionamiento de google si

--- build-a-bot/src/parts/PartInfo.vue
+++ build-a-bot/src/parts/PartInfo.vue
@@ -7,15 +7,26 @@
 	</div>
 </template>
 <script>
+	import parts from '../data/parts';
 	export default {
 		name: 'PartInfo',
-		data(){
-			return {
-				part:{
-					title: 'Part title',
-					description: 'Part description'
+		props:{
+			partType: {
+				type: String
+			},
+			id:{
+				type: [Number, String],
+				validator(value){
+					return Number.isInteger(Number(value));
 				}
-			};
+			}
+		},
+		computed:{
+			part:function(){
+				var partType = this.partType;
+				var id = this.id;
+				return parts[partType].find(part => part.id == +id);
+			}
 		}
 	};
 </script>
--- build-a-bot/src/router/index.js
+++ build-a-bot/src/router/index.js
@@ -3,7 +3,14 @@ import Router from 'vue-router';
 import HomePage from '../home/HomePage.vue';
 import RobotBuilder from '../build/RobotBuilder.vue';
 import PartInfo from '../parts/PartInfo.vue';
-
+import BrowseParts from '../parts/BrowseParts.vue';
+import RobotHeads from '../parts/RobotHeads.vue';
+import RobotArms from '../parts/RobotArms.vue';
+import RobotTorsos from '../parts/RobotTorsos.vue';
+import RobotBases from '../parts/RobotBases.vue';
+import SidebarStandard from '../sidebars/SidebarsStandard.vue';
+import SidebarBuild from '../sidebars/SidebarsBuild.vue';
+ 
 Vue.use(Router);
 
 export default new Router({
@@ -11,17 +18,51 @@ export default new Router({
 		{
 			path: '/',
 			name: 'Home',
-			component: HomePage
+			components: {
+				default: HomePage,
+				sidebar: SidebarStandard
+			}
 		},
 		{
 			path: '/build',
 			name: 'Build',
-			component: RobotBuilder
+			components: {
+				default: RobotBuilder,
+				sidebar: SidebarBuild
+			}
 		},
 		{
-			path: '/parts',
+			path: '/parts/browse',
+			name: 'BrowseParts',
+			component: BrowseParts,
+			children:[
+				{
+					name: 'BrowseHeads',
+					path:'heads',
+					component: RobotHeads
+				},
+				{
+					name: 'BrowseArms',
+					path:'arms',
+					component: RobotArms
+				},
+				{
+					name: 'BrowseTorsos',
+					path:'torsos',
+					component: RobotTorsos
+				},
+				{
+					name: 'BrowseBases',
+					path:'bases',
+					component: RobotBases
+				}
+			]
+		},
+		{
+			path: '/parts/:partType/:id',
 			name: 'Parts',
-			component: PartInfo
+			component: PartInfo,
+			props: true
 		}
 	]
 });
